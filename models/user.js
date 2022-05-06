b5ec71c12df604a02afa926560e691dda840525d
keyur patel <63536896+keyurpatel72@users.noreply.github.com>
2022-05-06 17:31:37 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,18 +1,42 @@
 const mongoose = require('mongoose');
 
 
-const Schema = mongoose.Schema
-
-const UserSchema = new Schema({
-    username: {
-        type: String,
-        required: [true, "username is required"]
-    }, 
-    password: {
-        type: String,
-        required: [true, "password is required"]
-    }
+const userSchema = new mongoose.Schema({
+    _id:mongoose.Schema.Types.ObjectId,
+    name:String,
+    email:String,
+    password:String,
+    role:String,
+    account:String
+    // name:{
+    //     type:String,
+    //     required:[true,"email is required"]
+    // },
+    // email: {
+    //     type: String,
+    //     required: [true, "email is required"]
+    // }, 
+    // password: {
+    //     type: String,
+    //     required: [true, "email is required"]
+    // }, 
+    // role: {
+    //     type: String,
+    //     required: [true, "email is required"]
+    // }, 
+    // account: {
+    //     type: String,
+    //     required: [true, "email is required"]
+    // },
+    //     date:{
+    //         type:Date,
+    //         default:Date.now
+    //     }
+    
 })
-const UserModel = mongoose.model("user", UserSchema)
+var User = mongoose.model("user", userSchema);
 
-module.exports = UserModel
+// const UserModel = mongoose.model("user", UserSchema)
+
+ module.exports = User
+
